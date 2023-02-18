fc59af1c3795bd411b40599db8a0f79f33122f55
zRick156 <robu608@gmail.com>
2023-02-18 22:30:37 +0100

    repo ordinata

---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rick <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 19:19:15 by rick              #+#    #+#             */
+/*   Updated: 2023/02/18 01:31:37 by rick             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+int	ft_controllo(int nb)
+{
+	if (nb == 0)
+	{
+		write(1, "0", 1);
+		return (0);
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		return (nb *= -1);
+	}
+	return (nb);
+}
+
+void	ft_putnbr(int nb)
+{
+	int	a;
+	int	contatore;
+	int	num[10];
+
+	nb = ft_controllo(nb);
+	contatore = 0;
+	a = nb;
+	while (a > 0)
+	{
+		a = a / 10;
+		++contatore;
+	}
+	a = contatore * 4;
+	while (contatore > 0)
+	{
+		num[contatore - 1] = nb % 10 + 48;
+		nb = nb / 10;
+		contatore--;
+	}
+	write(1, &num, a);
+}
+
+int	main(void)
+{
+	ft_putnbr(-42);
+}
