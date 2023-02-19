6320491e6322917ae69fbe955f6add76564fee86
zRick156 <robu608@gmail.com>
2023-02-19 14:04:34 +0100

    update

--- c00/ex03/ft_print_numbers.c
+++ c00/ex03/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: rick <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 18:13:08 by rick              #+#    #+#             */
-/*   Updated: 2023/02/19 00:33:50 by rick             ###   ########.fr       */
+/*   Updated: 2023/02/19 14:02:24 by rick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 void	ft_print_numbers(void)
 {
-	int	n;
-	int	a;
+	int	prt_num;
 
-	n = 0;
-	while (n <= 9)
+	prt_num = 48;
+	while (prt_num <= 57)
 	{
-		a = n + 48;
-		write (1, &a, 1);
-		n++;
+		write (1, &prt_num, 1);
+		prt_num++;
 	}
+}
+
+int	main(void)
+{
+	ft_print_numbers();
 }
